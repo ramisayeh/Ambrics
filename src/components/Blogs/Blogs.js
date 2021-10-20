731b1144be92276c519c44b72116646f9c1e3693
BugX <dhaouiaziz13@gmail.com>
2021-10-20 15:44:50 +0100

    certification done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,98 +1,112 @@
 import "./Blogs.css";
-import '../workandservices/ws.css'
-import work from '../../assets/work.jpg';
-import ITEMS from '../../assets/items.jpg';
-import AI from '../../assets/AI.jpg';
-import VR from '../../assets/VR.jpg';
+import "../workandservices/ws.css";
+import work from "../../assets/work.jpg";
+import ITEMS from "../../assets/items.jpg";
+import AI from "../../assets/AI.jpg";
+import VR from "../../assets/VR.jpg";
 
 export default function Blogs() {
   return (
-    <div className="blog-cont" >
-    <div className='blogs-text-container'>
-        <h2>Blogs</h2>
-    </div>
-    <div className='blogs-images'>
-    <figure className="snip1033">
-  <img src={ITEMS} alt="sample20" />
-  <figcaption>
-    <div className="left">
-      <h3>read</h3>
-    </div>
-    <div className="right">
-      <h3 className="white">blog</h3>
-    </div>
-  </figcaption>
-  <div className="center"><i className="ion-ios-loop-strong"></i></div>
-  <a href="#"></a>
-</figure>
-<figure className="snip1033">
-  <img src={work} alt="sample20" />
-  <figcaption>
-    <div className="left">
-      <h3>read</h3>
-    </div>
-    <div className="right">
-      <h3 className="white">blog</h3>
-    </div>
-  </figcaption>
-  <div className="center"><i className="ion-ios-loop-strong"></i></div>
-  <a href="#"></a>
-</figure>
-<figure className="snip1033">
-  <img src={AI} alt="sample20" />
-  <figcaption>
-    <div className="left">
-      <h3>read</h3>
-    </div>
-    <div className="right">
-      <h3 className="white">blog</h3>
-    </div>
-  </figcaption>
-  <div className="center"><i className="ion-ios-loop-strong"></i></div>
-  <a href="#"></a>
-</figure>
-<figure className="snip1033">
-  <img src={VR} alt="sample20" />
-  <figcaption>
-    <div className="left">
-      <h3>read</h3>
-    </div>
-    <div className="right">
-      <h3 className="white">blog</h3>
-    </div>
-  </figcaption>
-  <div className="center"><i className="ion-ios-loop-strong"></i></div>
-  <a href="#"></a>
-</figure> 
-    </div>
-    <div className='blogs-text-container2'>
-        <h2>Let's keep in touch, tell us about your business and the goals you wish to achieve from<br/>
-          our services, and we will make those goals a reality</h2>
-    </div><br/>
-    <br/>
-    <br/>
-    <div className='contact-container'>
-      <div className='contact-text'>
-        <h3>Let's keep in touch!<br/> 
-        Subscribe to keep up with fresh news and exciting updates.<br/>
-        We promise not to spam you!</h3>
+    <div className="blog-cont">
+      <div className="blogs-text-container">Blogs</div>
+      <div className="blogs-images">
+        <figure className="snip1033">
+          <img src={ITEMS} alt="sample20" />
+          <figcaption>
+            <div className="left">
+              <h3>read</h3>
+            </div>
+            <div className="right">
+              <h3 className="white">blog</h3>
+            </div>
+          </figcaption>
+          <div className="center">
+            <i className="ion-ios-loop-strong"></i>
+          </div>
+          <a href="#"></a>
+        </figure>
+        <figure className="snip1033">
+          <img src={work} alt="sample20" />
+          <figcaption>
+            <div className="left">
+              <h3>read</h3>
+            </div>
+            <div className="right">
+              <h3 className="white">blog</h3>
+            </div>
+          </figcaption>
+          <div className="center">
+            <i className="ion-ios-loop-strong"></i>
+          </div>
+          <a href="#"></a>
+        </figure>
+        <figure className="snip1033">
+          <img src={AI} alt="sample20" />
+          <figcaption>
+            <div className="left">
+              <h3>read</h3>
+            </div>
+            <div className="right">
+              <h3 className="white">blog</h3>
+            </div>
+          </figcaption>
+          <div className="center">
+            <i className="ion-ios-loop-strong"></i>
+          </div>
+          <a href="#"></a>
+        </figure>
+        <figure className="snip1033">
+          <img src={VR} alt="sample20" />
+          <figcaption>
+            <div className="left">
+              <h3>read</h3>
+            </div>
+            <div className="right">
+              <h3 className="white">blog</h3>
+            </div>
+          </figcaption>
+          <div className="center">
+            <i className="ion-ios-loop-strong"></i>
+          </div>
+          <a href="#"></a>
+        </figure>
       </div>
-      <br/>
-      <div className='input-container'>
-      <input className='contact-input'/> <br/>
-      <button className="contactb"> Hover me</button>
-      <br/>
+      <div className="blogs-text-container2">
+        <h2>
+          Let's keep in touch, tell us about your business and the goals you
+          wish to achieve from
+          <br />
+          our services, and we will make those goals a reality
+        </h2>
       </div>
-      <br/>
-      <div id='checkbox-container' > 
-      <input type="checkbox" id="box-1"/>
-  <label id='newsletter-label' for="box-1">I agree to my email address being stored and used to receive DAN monthly newsletter. </label>
-  </div>
-    </div> 
-
+      <br />
+      <br />
+      <br />
+      <div className="contact-container">
+        <div className="contact-text">
+          <h3>
+            Let's keep in touch!
+            <br />
+            Subscribe to keep up with fresh news and exciting updates.
+            <br />
+            We promise not to spam you!
+          </h3>
+        </div>
+        <br />
+        <div className="input-container">
+          <input className="contact-input" /> <br />
+          <button className="contactb"> Hover me</button>
+          <br />
+        </div>
+        <br />
+        <div id="checkbox-container">
+          <input type="checkbox" id="box-1" />
+          <label id="newsletter-label" htmlFor="box-1">
+            I agree to my email address being stored and used to receive DAN
+            monthly newsletter.{" "}
+          </label>
+        </div>
+      </div>
     </div>
   );
 }
-
-
-
